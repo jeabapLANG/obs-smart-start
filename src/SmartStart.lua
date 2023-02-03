17da2b49092adf959acdda31b721501e706099dd
takorojo <112791133+takorojo@users.noreply.github.com>
2023-02-03 00:51:21 -0800

    Simplify check_os() code
    I've just learned that OBS already embeds LuaJIT, so I don't have to worry about
    checking for environment variables and such.  A lot of that code was removed as
    a result.
    
    Also, we didn't really need to test for what architecture the system is running
    since we're only worried about what OS the user is running.  Architecture
    testing code has been removed as well.

--- src/SmartStart.lua
+++ src/SmartStart.lua
@@ -35,32 +35,9 @@ ScriptDescription = [[
 
 -- Check the user's OS.  Outputs OS and architecture as an array.
 function check_os()
-    local raw_os_name, raw_arch_name = '', ''
-
-    -- LuaJIT shortcut
-    if jit and jit.os and jit.arch then
-        raw_os_name = jit.os
-        raw_arch_name = jit.arch
-    else
-        -- is popen supported?
-        local popen_status, popen_result = pcall(io.popen, "")
-        if popen_status then
-            popen_result:close()
-            -- Unix-based OS
-            raw_os_name = io.popen('uname -s', 'r'):read('*l')
-            raw_arch_name = io.popen('uname -m', 'r'):read('*l')
-        else
-            -- Windows
-            local env_OS = os.getenv('OS')
-            local env_ARCH = os.getenv('PROCESSOR_ARCHITECTURE')
-            if env_OS and env_ARCH then
-                raw_os_name, raw_arch_name = env_OS, env_ARCH
-            end
-        end
-    end
+	local raw_os_name = jit.os
 
     raw_os_name = (raw_os_name):lower()
-    raw_arch_name = (raw_arch_name):lower()
 
     local os_patterns = {
         ['windows'] = "Windows",
@@ -74,17 +51,7 @@ function check_os()
         ['sunos'] = 'Solaris',
     }
 
-    local arch_patterns = {
-        ['^x86$'] = 'x86',
-        ['i[%d]86'] = 'x86',
-		['amd64'] = 'x86_64',
-		['x86_64'] = 'x86_64',
-		['Power Macintosh'] = 'powerpc',
-		['^arm'] = 'arm',
-		['^mips'] = 'mips',
-    }
-
-    local os_name, arch_name = 'unknown', 'unknown'
+	local os_name = 'unknown'
 
     for pattern, name in pairs(os_patterns) do
         if raw_os_name:match(pattern) then
@@ -93,17 +60,10 @@ function check_os()
         end
     end
 
-    for pattern, name in pairs(arch_patterns) do
-        if raw_arch_name:match(pattern) then
-            arch_name = name
-            break
-        end
-    end
-
-    return os_name, arch_name
+	return os_name
 end
 
-USER_OS_NAME, USER_OS_ARCH = check_os()
+USER_OS_NAME = check_os()
 
 -- A function named script_defaults will be called to set the default settings
 function script_defaults(settings)
